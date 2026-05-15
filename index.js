dd9c379ca7380fc9864a61f4e9b78346d688bc08
curso-git <k.vargasb2004@gmail.com>
2026-05-15 05:14:41 -0500

    Implementa consulta relacional (JOIN) entre estudiantes y materias

--- index.js
+++ index.js
@@ -5,16 +5,35 @@ const supabaseKey = 'sb_publishable_Wh-g6of5yaooeExy-a-s5Q_dLiPrzlP'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function obtenerEstudiantes() {
+async function consultarSistemaUniversitario() {
+  console.log('Consultando base de datos...\n')
+
+  
   const { data, error } = await supabase
     .from('estudiantes')
-    .select('*')
+    .select(`
+      Nombre,
+      Carrera,
+      materias (
+        "nombre materia",
+        docente
+      )
+    `)
 
   if (error) {
-    console.log('Error:', error)
+    console.log('Error en la consulta:', error)
   } else {
-    console.log('Datos:', data)
+    console.log('--- REPORTE DE INSCRIPCIONES ---')
+    data.forEach(registro => {
+      const materia = registro.materias ? registro.materias['nombre materia'] : 'Sin materia'
+      const profe = registro.materias ? registro.materias.docente : 'N/A'
+      
+      console.log(`- Estudiante: ${registro.Nombre}`)
+      console.log(`  Carrera: ${registro.Carrera}`)
+      console.log(`  Materia: ${materia} (Docente: ${profe})`)
+      console.log('-----------------------------------')
+    })
   }
 }
 
-obtenerEstudiantes()
+consultarSistemaUniversitario()
